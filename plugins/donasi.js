1544d01440a6c9b2b6adfe9ebab88c26d574a574
candranet <candranetprogram@gmail.com>
2022-04-11 11:20:48 +0700

    first commit

---
+++ plugins/donasi.js
@@ -0,0 +1,15 @@
+//Jangan Di Hapus Tapi kalo Di tambah Boleh
+
+let fetch = require('node-fetch')
+let handler = async (m, { conn }) => await conn.sendButtonLoc(m.chat, await (await fetch(thumbfoto)).buffer(), `
+┌〔 Donasi • Emoney 〕
+├ GoPay: 089670004720 
+├ Dana: 089670004720 
+├ Pulsa: 089670004720 
+└────
+`.trim(), watermark, 'Menu', '.menu', m)
+handler.help = ['donasi']
+handler.tags = ['info']
+handler.command = /^dona(te|si)$/i
+
+module.exports = handler
---
+++ plugins/snk.js
@@ -0,0 +1,39 @@
+let fetch = require('node-fetch')
+let handler = async(m, { conn }) => {
+    let teks = `
+Kebijakan Privasi, Syarat Ketentuan dan Peraturan zaan Bot
+
+Kebijakan Privasi
+1. zaan tidak akan merekam data riwayat chat user.
+2. zaan tidak akan menyebarkan nomor users.
+3. zaan tidak akan menyimpan media yang dikirimkan oleh users.
+4. zaan tidak akan menyalah gunakan data data users.
+5. Owner zaan berhak melihat data riwayat chat users.
+6. Owner zaan berhak melihat status users.
+7. Owner zaan dapat melihat riwayat chat, dan media yang dikirimkan users.
+
+Peraturan zaan Bot
+1. Users dilarang menelpon maupun memvideo call nomor bot.
+2. Users dilarang mengirimkan berbagai bug, virtex, dll ke nomor bot.
+3. Users diharap tidak melakukan spam dalam penggunaan bot.
+4. Users dilarang menambahkan nomor bot secara illegal, untuk menambahkan silahkan hubungi owner.
+5. Users diharap untuk tidak menyalah gunakan fitur fitur bot.
+
+Syarat Ketentuan zaan Bot
+1. Bot akan keluar dari group apabila sudah waktunya keluar.
+2. zaan dapan mem-ban users secara sepihak terlepas dari users salah atau tidak.
+3. zaan *tidak akan bertanggungjawab atas apapun yang users lakukan terhadap fitur bot.*
+4. zaan akan memberlakukan hukuman: block atau ban terhadap users yang melanggar peraturan.
+5. zaan bertanggung jawab atas kesalahan fatal dalam programing maupun owner.
+
+- Candra
+- Afzaan
+
+Peraturan: 12 april 2022
+`.trim()
+    conn.send2ButtonLoc(m.chat, await(await fetch(image)).buffer(), teks, watermark, 'Menu', '.menu', 'Owner', '.owner', m)
+}
+handler.help = ['peraturan']
+handler.command = /^(snk|syarat|peraturan|rules)$/i
+handler.tags = ['main']
+module.exports = handler
